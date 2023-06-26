91837ff5e716e768f9a4bf876eaf737d4b1a31bc
abdallah279 <abdallahzeko8@gmail.com>
2023-06-26 13:49:30 +0300

    responsive

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,32 +19,27 @@ $(".page-navbar .links .link").each(function () {
   }
 });
 
-// Show And Hide Search Navbar
-$('.nav-search-ic').on('click', function(){
-$('.nav-search').toggleClass('open');
-});
-
 
 // SideBar
 $('.side-open').on('click', function(){
-$('.links').addClass('active');
-$('.overlay-m').fadeIn(600);
+  $('.links').addClass('active');
+  $('.overlay-m').fadeIn(600);
 });
 
-$('.side-user-open').on('click', function(){
-$('.sidebar').addClass('active');
-$('.overlay-m').fadeIn(600);
-});
+// $('.side-user-open').on('click', function(){
+//   $('.sidebar').addClass('active');
+//   $('.overlay-m').fadeIn(600);
+// });
 
-$('.close').on('click', function(){
-$('.links').removeClass('active');
-$('.overlay-m').fadeOut(500);
-});
+// $('.close').on('click', function(){
+//   $('.links').removeClass('active');
+//   $('.overlay-m').fadeOut(500);
+// });
 
 $('.overlay-m').on('click', function(){
-$('.links').removeClass('active');
-$('.sidebar').removeClass('active');
-$(this).fadeOut(500);
+  $('.links').removeClass('active');
+  // $('.sidebar').removeClass('active');
+  $(this).fadeOut(500);
 });
 
 
@@ -54,32 +49,6 @@ $(".dropdown-menu").click(function(e){
 });
 
 
-// PassWord Show In Setting Page
-const iconsPassSet = document.querySelectorAll('.pass-icon');
-
-if(iconsPassSet){
-iconsPassSet.forEach((ic) =>{
-  ic.addEventListener('click', function(){
-    let input = ic.parentElement.querySelector('input');
-    showPassword(input, ic);
-  });
-});
-}
-
-// Function To Show And Hide Password
-function showPassword(input, icon){
-
-if(input.type == 'password'){
-  input.setAttribute('type', 'text');
-  // icon.innerHTML = `<i class="fa-regular fa-eye"></i>`;
-} else{
-  input.setAttribute('type', 'password');
-  // icon.innerHTML = `<i class="fa-regular fa-eye-slash"></i>`;
-}
-
-icon.classList.toggle('show')
-}
-
 let isRtl = $('html[lang="ar"]').length > 0;
 
 // Normal Select To
@@ -87,41 +56,3 @@ $(".select").select2({
 dir: isRtl ? "rtl" : "ltr",
 minimumResultsForSearch: Infinity,
 });
-
-
-
-// Heart
-$(document).on('click', '.heart' ,function(){
-if($(this).find('i').hasClass('fa-regular')){
-  $(this).find('i').addClass('fa-solid yellow')
-  $(this).find('i').removeClass('fa-regular')
-} else{
-  $(this).find('i').removeClass('fa-solid yellow')
-  $(this).find('i').addClass('fa-regular')
-}
-})
-
-
-// $('[data-pass]').on('click', function(){
-// let item = $(this).attr('data-pass')
-// sessionStorage.setItem("activeTab", JSON.stringify(item));
-// })
-
-let allCopy = document.querySelectorAll('.circle')
-
-allCopy.forEach(el => {
-  el.addEventListener('click', function(){
-      let code = el.closest('.copy-item').querySelector('.copy-num');
-      CopyToClipboard(code);
-  });
-})
-
-
-function CopyToClipboard(id) {
-  var r = document.createRange();
-  r.selectNode(id);
-  window.getSelection().removeAllRanges();
-  window.getSelection().addRange(r);
-  document.execCommand('copy');
-  window.getSelection().removeAllRanges();
-}
